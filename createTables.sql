037ca48cd2e4d35f0ed7b0ba603025254a5ceb75
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-03 06:44:42 -0600

    Update createTables.sql
    additional tables

--- createTables.sql
+++ createTables.sql
@@ -22,6 +22,64 @@ CREATE TABLE teamLeads (
 
 CREATE TABLE teams (
   id SERIAL PRIMARY KEY,
+  teamName VARCHAR(50),
   teamLead_id INTEGER REFERENCES teamLeads(id),
   contractor_id INTEGER REFERENCES contractors(id)
 );
+
+CREATE TABLE clients (
+  id SERIAL PRIMARY KEY,
+  firstName VARCHAR(50) NOT NULL,
+  lastName VARCHAR(50) NOT NULL,
+  addressStreet VARCHAR(50) NOT NULL,
+  addressCity VARCHAR(50) NOT NULL,
+  addressState VARCHAR(50) NOT NULL,
+  phoneNumber INTEGER NOT NULL,
+  email VARCHAR(50),
+  onboardDate DATE,
+  marketMethod VARCHAR(50),
+  frequency  VARCHAR(50),
+  dayOfWeek VARCHAR(50),
+  timeOfDay CHAR(2),
+  bidValue INTEGER NOT NULL,
+  team_id INTEGER REFERENCES teams(id)
+)
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  prductName VARCHAR(50) NOT NULL,
+  ProductType VARCHAR(50),
+  cost INTEGER NOT NULL,
+  suplier_id INTEGER REFERENCES supliers(id)
+)
+
+CREATE TABLE supliers (
+  id SERIAL PRIMARY KEY,
+  suplierName VARCHAR(50) NOT NULL,
+  suplierUrl VARCHAR(50),
+)
+
+CREATE TABLE inventory (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER REFERENCES products(id),
+  onHand INTEGER
+)
+
+CREATE TABLE assets (
+  id SERIAL PRIMARY KEY,
+  assetType VARCHAR(50),
+  assetName VARCHAR(50) NOT NULL,
+  cost INTEGER NOT NULL,
+  purchaseDate DATE,
+  serialNumber VARCHAR(50) NOT NULL,
+  suplier_id INTEGER REFERENCES supliers(id),
+  contractor_id INTEGER REFERENCES contractors(id)
+)
+
+CREATE TABLE marketing (
+  id SERIAL PRIMARY KEY,
+  marketerName VARCHAR(50) NOT NULL,
+  marketerType VARCHAR(50),
+  marketerUrl VARCHAR(50),
+
+)
